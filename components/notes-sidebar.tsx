2bc34836236aba95fe2cfe23c65f4812e9104d89
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-24 15:38:46 +0530

    storage.ts file added

--- components/notes-sidebar.tsx
+++ components/notes-sidebar.tsx
@@ -7,6 +7,7 @@ import {
   } from "@/components/ui/card"
 import EmptyState from './empty-state';
 import { Note } from '@/lib/types';
+import { Button } from './ui/button';
 
 
 interface NotesSidebarProps {
@@ -14,23 +15,45 @@ interface NotesSidebarProps {
 }
 
   
-function NotesSidebar({notes}: NotesSidebarProps) {
-  
+function NotesSidebar({ notes }: NotesSidebarProps) {
   return (
     <Card>
-    <CardHeader>
-      <CardTitle>My Notes</CardTitle>
-    </CardHeader>
-    <CardContent>
-    {notes.length === 0 ? (
-        <EmptyState message='No Notes Yet ' buttonText='Create your first note'/>
-    ):(
-        <h1>list of the notes</h1>
-    )}
-    </CardContent>
-  </Card>
-  
-  )
+      <CardHeader>
+        <CardTitle>My Notes</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {notes.length === 0 ? (
+          <EmptyState message="No Notes Yet" buttonText="Create your first note" />
+        ) : (
+          <div>
+            {notes.map((note) => (
+              <div
+                key={note.id}
+                className="p-3 rounded-md cursor-pointer hover:bg-accent transition-colors"
+              >
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h3 className="font-medium">
+                      {note.title.substring(0, 30)}
+                      {note.title.length > 30 ? '...' : ''}
+                    </h3>
+                    <p className='text-sm text-muted-foreground'>
+                      {note.content.substring(0, 40)}
+                      {note.content.length > 40 ? '...' : ''}
+                    </p>
+                    <p className='text-sm text-muted-foreground'>
+                     {note.createdAt}
+                     </p>
+                  </div>
+                  <Button>remove</Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
 
-export default NotesSidebar
+export default NotesSidebar;
---
+++ lib/storage.ts
@@ -0,0 +1,7 @@
+export function formatDate(timestamp:number):string{
+    return new Date(timestamp).toLocaleDateString('en-US', {
+        year:"numeric",
+        month:"short",
+        day:"numeric"
+    })
+}
